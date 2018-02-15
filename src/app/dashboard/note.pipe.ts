c6b312d43545f776229b8e15438ca27cc28626b3
Anthony Kluba <anthonykluba@gmail.com>
2018-02-15 11:57:23 -0500

    adding new pipe.

---
+++ src/app/dashboard/note.pipe.ts
@@ -0,0 +1,43 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'noteFilter'
+})
+export class NoteFilterPipe implements PipeTransform {
+    transform(notes: any[], filter: any): any[] {
+        if (!notes || (!filter.class && !filter.field)) {
+            return notes;
+        }
+
+        if (filter.field) {
+            return notes.filter(note => note.filters.field === filter.field);
+        } else {
+            return notes.filter(note => note.filters.class === filter.class);
+        }
+    }
+}
+
+
+@Pipe({
+    name: 'noteSort'
+})
+export class NoteSortPipe implements PipeTransform {
+    transform(array: any[], args: string): any[] {
+        return array.sort(this.compare);
+    }
+
+    compare(a, b): number {
+        const sortA = a.created_date;
+        const sortB = b.created_date;
+
+        let comparison = 0;
+
+        if (sortA > sortB) {
+            comparison = -1;
+        } else if (sortA < sortB) {
+            comparison = 1;
+        }
+
+        return comparison;
+    }
+}
