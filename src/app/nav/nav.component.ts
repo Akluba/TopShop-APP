ecc6208af1882c5d51d0b0cef167917829ad1d77
Anthony Kluba <anthonykluba@gmail.com>
2018-02-21 16:04:24 -0500

    Rearranging account nav items into sub menu.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../core/auth.service';
+
+declare let $: any;
+
 @Component({
     selector: 'app-nav',
-    templateUrl: 'nav.component.html'
+    templateUrl: 'nav.component.html',
+    styles: [
+        `
+            .ui.sidebar { overflow: visible !important; }
+        `
+    ]
 })
-export class NavComponent {
+export class NavComponent implements AfterViewInit {
     constructor(public authService: AuthService, private _router: Router) {}
+
+    ngAfterViewInit(): void {
+        $('.dropdown').dropdown();
+    }
 
     logout(): void {
         this.authService.logout()
