a3700a581e3613a6b36a7a3d2c240fc5957b2e0d
Anthony Kluba <anthonykluba@gmail.com>
2018-03-05 13:54:17 -0500

    Fixing merge conflict.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,6 @@ import { PageNotFoundComponent } from './pagenotfound.component';
 export const routes: Routes = [
     { path: 'dash', canLoad: [ AuthGuard ], loadChildren: 'app/dashboard/dash.module#DashModule' },
     { path: 'setup', canLoad: [ AuthGuard ], loadChildren: 'app/setup/setup.module#SetupModule' },
-<<<<<<< HEAD
-    { path: 'search', canLoad: [ AuthGuard ], loadChildren: 'app/search/search.module#SearchModule' },
-    { path: 'shops', canLoad: [ AuthGuard ], loadChildren: 'app/shops/shop.module#ShopModule' },
-    { path: 'managers', canLoad: [ AuthGuard ], loadChildren: 'app/managers/manager.module#ManagerModule' },
-    { path: 'vendors', canLoad: [ AuthGuard ], loadChildren: 'app/vendors/vendor.module#VendorModule' },
-=======
     {
         path: 'shops',
         canLoad: [ AuthGuard, ProfileGuard ],
@@ -40,7 +34,6 @@ export const routes: Routes = [
         loadChildren: 'app/cpr/cpr.module#CPRModule',
         data: { authorizedProfiles: ['admin', 'cpr'] }
     },
->>>>>>> origin/enhance_6
     { path: 'unauthorized', component: UnauthorizedComponent },
     { path: '', redirectTo: 'dash', pathMatch: 'full'},
     { path: '**', component: PageNotFoundComponent }
