1bd338ad4a308663457d3928fde0c1faa00ac25f
akluba-at-427331217318 <anthonykluba@gmail.com>
2022-01-05 14:36:56 -0500

    Added hash location so apps functions when refreshing in browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { UnauthorizedComponent } from './unauthorized.component';
@@ -28,6 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
     UnauthorizedComponent,
     PageNotFoundComponent
   ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
